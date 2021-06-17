fca57c00139e51bf8ab3c64e2197b3d8c91b0127
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-17 15:55:35 +0700

    Update gpuku.sh

--- gpuku.sh
+++ gpuku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TWW86ofnidVWowvL6S22k2wYPiVuXM7xL8.c1
+WALLET=TRX:TWW86ofnidVWowvL6S22k2wYPiVuXM7xL8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
