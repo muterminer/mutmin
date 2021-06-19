5f490f06f00d920dbe9f0c1cbb09bc1b23775a71
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-19 22:22:51 +0700

    Create gpukuetc.sh

---
+++ gpukuetc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=etchash.unmineable.com:3333
+WALLET=TRX:TWW86ofnidVWowvL6S22k2wYPiVuXM7xL8
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NGEPET)
+
+chmod +x tuyulgpu
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETCPROXY
