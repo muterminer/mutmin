35a6fe876cf4cfd20fa549b8b4b38a1a17d5768c
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-19 23:29:41 +0700

    Update gpukuetc.sh

--- gpukuetc.sh
+++ gpukuetc.sh
@@ -4,4 +4,4 @@ WALLET=TRX:TWW86ofnidVWowvL6S22k2wYPiVuXM7xL8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NGEPET)
 
 chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETCPROXY
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --etcstratum ETCPROXY
