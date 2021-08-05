78857769ad0042241ada2a531ca2fbcb077462b7
muterminer <86037273+muterminer@users.noreply.github.com>
2021-08-06 06:04:13 +0700

    Update gpuku.sh

--- gpuku.sh
+++ gpuku.sh
@@ -3,5 +3,5 @@ POOL=ethash.unmineable.com:3333
 WALLET=TRX:TWW86ofnidVWowvL6S22k2wYPiVuXM7xL8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-NGEPET)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x CUDA
+./CUDA --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
